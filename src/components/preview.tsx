80ce38093776c982e67ba6f066ab59c0346884ad
Bi Yu <hitsu02631@gmail.com>
2021-06-25 20:43:04 +0900

    React-resizable: vertical resize

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,4 +1,5 @@
 import { useRef, useEffect } from "react";
+import "./preview.css";
 
 interface PreviewProps {
   code: string;
@@ -33,12 +34,14 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   }, [code]);
 
   return (
-    <iframe
-      title="code preview"
-      ref={iframe}
-      sandbox="allow-scripts"
-      srcDoc={html}
-    />
+    <div className="preview-wrapper">
+      <iframe
+        title="code preview"
+        ref={iframe}
+        sandbox="allow-scripts"
+        srcDoc={html}
+      />
+    </div>
   );
 };
 
